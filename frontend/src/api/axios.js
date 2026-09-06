d90e3d315043f291714eb5f8e69867cca520cb91
PriyaS876 <priya.rani@lpu.in>
2026-09-06 21:05:38 +0530

    Connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://ticket-booking-system-u8k9.onrender.com',
   timeout: 10000, // 10 seconds mein response na aaye to timeout error
 });
 
